54801d387f9db482ba6200ab00de0cbf6f92da8f
Gabriel CAPELL <capell.gabriel@gmail.com>
2022-12-12 09:07:56 +0000

    Ajout BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -19,6 +19,8 @@ class BlogController extends AbstractController
     #[Route('/', name: 'home')]
     public function home()
     {
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig', [
+            'title' => 'Bienvenue ici'
+        ]);
     }
 }
